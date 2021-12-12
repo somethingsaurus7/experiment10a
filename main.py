e2fbf92b262462b5fb26a83d3444b8b12e6550ac
somethingsaurus7 <96009468+somethingsaurus7@users.noreply.github.com>
2021-12-12 13:34:42 +0000

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 Blue = 0
 Green = 0
 Red = 0
+
+def on_forever():
+    global Green, Red, Blue
+    pins.analog_write_pin(AnalogPin.P0, Red)
+    pins.analog_write_pin(AnalogPin.P1, Green)
+    pins.analog_write_pin(AnalogPin.P2, Blue)
+    if pins.digital_read_pin(DigitalPin.P8) == 1 and Green <1020:
+        Green = Green +10
+    elif pins.digital_read_pin(DigitalPin.P8) == 1 and Green == 1020:
+        Green = 0
+    if pins.digital_read_pin(DigitalPin.P8) == 1 and Red <1020:
+        Red = Red +10
+    elif pins.digital_read_pin(DigitalPin.P8) == 1 and Red == 1020:
+        Red = 0
+    if pins.digital_read_pin(DigitalPin.P8) == 1 and Blue <1020:
+        Blue = Blue +10
+    elif pins.digital_read_pin(DigitalPin.P8) == 1 and Blue == 1020:
+        Blue = 0
+basic.forever(on_forever)
