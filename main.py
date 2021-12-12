1f20c6026dbd02d56d7b7bc9a45cfd3ab98f3a88
somethingsaurus7 <96009468+somethingsaurus7@users.noreply.github.com>
2021-12-12 13:39:18 +0000

    Update pxt.json, main.py

--- main.py
+++ main.py
@@ -7,16 +7,16 @@ def on_forever():
     pins.analog_write_pin(AnalogPin.P0, Red)
     pins.analog_write_pin(AnalogPin.P1, Green)
     pins.analog_write_pin(AnalogPin.P2, Blue)
-    if pins.digital_read_pin(DigitalPin.P8) == 1 and Green <1020:
-        Green = Green +10
+    if pins.digital_read_pin(DigitalPin.P8) == 1 and Green < 1020:
+        Green = Green + 10
     elif pins.digital_read_pin(DigitalPin.P8) == 1 and Green == 1020:
         Green = 0
-    if pins.digital_read_pin(DigitalPin.P8) == 1 and Red <1020:
-        Red = Red +10
+    if pins.digital_read_pin(DigitalPin.P8) == 1 and Red < 1020:
+        Red = Red + 10
     elif pins.digital_read_pin(DigitalPin.P8) == 1 and Red == 1020:
         Red = 0
-    if pins.digital_read_pin(DigitalPin.P8) == 1 and Blue <1020:
-        Blue = Blue +10
+    if pins.digital_read_pin(DigitalPin.P8) == 1 and Blue < 1020:
+        Blue = Blue + 10
     elif pins.digital_read_pin(DigitalPin.P8) == 1 and Blue == 1020:
         Blue = 0
 basic.forever(on_forever)
